96abb6770c14ada6370e29f99752d2553eaa4d03
Muala Kabashi <keds30955@KEDS.kv>
2024-12-26 15:40:17 +0100

    add navigation drawer

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,9 +44,6 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
-    buildFeatures{
-        viewBinding
-    }
     viewBinding{
         enable = true
     }
--- app/src/main/java/com/cacttus/navigationdrawergr_3/fragments/ProfileFragment.kt
+++ app/src/main/java/com/cacttus/navigationdrawergr_3/fragments/ProfileFragment.kt
@@ -5,8 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.cacttus.navigationdrawergr_3.databinding.HomeFragmentBinding
 import com.cacttus.navigationdrawergr_3.databinding.ProfileFragmentBinding
+
 
 class ProfileFragment : Fragment() {
     private lateinit var binding: ProfileFragmentBinding
@@ -15,9 +15,6 @@ class ProfileFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        //opsioni 1
-//        return layoutInflater.inflate(R.layout.home_fragment, container, false)
-        //opsioni 2
         binding = ProfileFragmentBinding.inflate(layoutInflater)
         return binding.root
     }
